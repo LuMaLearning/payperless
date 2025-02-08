01cc7827b19f11156eaa3b78468674214b5a9d81
Brendon Hablutzel <booklvrbren@gmail.com>
2025-02-08 13:34:33 -0500

    add pydantic type example

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from typing import Union
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
@@ -10,6 +11,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+class ItemResponse(BaseModel):
+    item_id: int
+    q: Union[str, None]
+
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None) -> ItemResponse:
     return {"item_id": item_id, "q": q}
